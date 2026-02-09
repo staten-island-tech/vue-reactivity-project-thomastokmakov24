dda442e244c515cdcf20fd74c9a79973a1ec6d9b
thomastokmakov24 <thomast89@nycstudents.net>
2026-02-09 12:32:50 -0500

    ggggg
    gg

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,8 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
+  <div><RouterView/></div>
+  
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
